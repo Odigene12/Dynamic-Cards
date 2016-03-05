a306edfdbf11b1d26341bdbe4ce954441f3f56c7
Odie Joseph <Odie Joseph>
2016-03-05 12:42:57 -0600

    Created elements and variables

---
+++ cards.js
@@ -0,0 +1,8 @@
+// create variables
+var create = document.getElementById("create")
+var input = document.getElementById("userInput")
+var contain = document.getElementById("containment")
+ //dynamic variables 
+var card = document.getElementsByClassName("card"); 
+var del = document.getElementById("delete")
+// end of dynamic variables
